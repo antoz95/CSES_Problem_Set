3c76a73cf88cdcd3d04f36d568f37b5bfd117df6
Anto Zacharias <57959232+antoz95@users.noreply.github.com>
2021-07-24 14:32:34 +0530

    Update and rename WeirdAlgorithm to WeirdAlgorithm.java

--- WeirdAlgorithm.java
+++ WeirdAlgorithm.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class WeirdAlgorithm {
     public static void main(String []args) {
         Scanner s = new Scanner(System.in);
-		    long n = s.nextLong();
-		    System.out.print(n+ " ");
+	long n = s.nextLong();
+	System.out.print(n+ " ");
         while(n>1) {
-	        if(n%2==0)
+		if(n%2==0)
 		        n = n/2;
-	        else if (n==1)
+	        else if (n == 1)
 		        n = 1;
 	        else
-		        n = (n*3)+1;
-		      System.out.print(n + " ");
-	      }
+		        n = (n*3) + 1;
+		System.out.print(n + " ");
+	}
     }
 }
