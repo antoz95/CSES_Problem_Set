7b904d298b9f077dc9049ec731cbef8a9e8aadbc
Anto Zacharias <57959232+antoz95@users.noreply.github.com>
2021-07-25 13:22:09 +0530

    Add files via upload

---
+++ MissingNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MissingNumber{
+     
+    public static void main(String []args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        long n = sc.nextLong();
+        long total = 0;
+        
+        for (int i=0; i<n-1; i++)
+            total = total + sc.nextLong();
+            
+        long sum = (n*(n+1))/2;
+        
+        System.out.println(sum - total);
+    }
+}
