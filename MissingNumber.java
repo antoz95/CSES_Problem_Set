6a6ce481120c425b884158a9d6c96f2c73564a8c
Anto Zacharias <57959232+antoz95@users.noreply.github.com>
2021-07-30 12:50:22 +0530

    Update MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-public class MissingNumber{
-     
-    public static void main(String []args){
-        
-        Scanner sc = new Scanner(System.in);
-        
-        long n = sc.nextLong();
-        long total = 0;
-        
-        for (int i=0; i<n-1; i++)
-            total = total + sc.nextLong();
-            
-        long sum = (n*(n+1))/2;
-        
-        System.out.println(sum - total);
-    }
+public class MissingNumber {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+
+		long total = 0;
+		for (int i = 0; i < n - 1; i++)
+			total = total + sc.nextLong();
+		long sum = (n * (n + 1)) / 2;
+		System.out.println(sum - total);
+
+		sc.close();
+	}
 }
