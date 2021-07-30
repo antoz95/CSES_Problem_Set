d1f0bd578641923918f3b7503c92bb2a9d3704b5
Anto Zacharias <57959232+antoz95@users.noreply.github.com>
2021-07-30 12:50:45 +0530

    Update WeirdAlgorithm.java

--- WeirdAlgorithm.java
+++ WeirdAlgorithm.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
 
 public class WeirdAlgorithm {
-    public static void main(String []args) {
-        Scanner s = new Scanner(System.in);
-	long n = s.nextLong();
-	System.out.print(n+ " ");
-        while(n>1) {
-		if(n%2==0)
-		        n = n/2;
-	        else if (n == 1)
-		        n = 1;
-	        else
-		        n = (n*3) + 1;
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+
 		System.out.print(n + " ");
+		while (n > 1) {
+			if (n % 2 == 0)
+				n = n / 2;
+			else if (n == 1)
+				n = 1;
+			else
+				n = (n * 3) + 1;
+			System.out.print(n + " ");
+		}
+
+		sc.close();
 	}
-    }
 }
